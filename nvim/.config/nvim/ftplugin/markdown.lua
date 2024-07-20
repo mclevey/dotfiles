5cdafb0548da9bc08e2faab70f8b46ee504d1697
John McLevey <john.mclevey@hey.com>
2024-07-20 13:43:45 -0230

    cleaned up the markdown experience in nvchad

---
+++ nvim/.config/nvim/ftplugin/markdown.lua
@@ -0,0 +1,7 @@
+-- ~/.config/nvim/ftplugin/markdown.lua
+vim.opt_local.wrap = true
+vim.opt_local.linebreak = true
+-- vim.opt_local.breakindent = true
+-- vim.opt_local.showbreak = '↪ '
+vim.opt_local.conceallevel = 2
+vim.opt_local.concealcursor = 'nc'
